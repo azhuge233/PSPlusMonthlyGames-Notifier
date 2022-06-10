3cc3e3324c994d4a068ec9bae8b883711051af2e
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2022-06-10 20:59:12 +0800

    添加项目文件。

---
+++ PSPlusMonthlyGames-Notifier/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace PSPlusMonthlyGames_Notifier {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
