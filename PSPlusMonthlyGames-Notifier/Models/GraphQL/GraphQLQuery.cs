051b4d2cf70b87c701a1931a4467549eaa1e5dba
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2026-01-08 13:11:58 +0800

    Add GraphQL serialize models

---
+++ PSPlusMonthlyGames-Notifier/Models/GraphQL/GraphQLQuery.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PSPlusMonthlyGames_Notifier.Models.GraphQL {
+	public class PersistedQuery {
+		[JsonPropertyName("version")]
+		public int Version { get; set; } = 1;
+
+		[JsonPropertyName("sha256Hash")]
+		public string Sha256Hash { get; set; }
+	}
+
+	public class Extensions {
+		[JsonPropertyName("persistedQuery")]
+		public PersistedQuery PersistedQuery { get; set; } = new();
+	}
+
+	public class PageArgs {
+		[JsonPropertyName("size")]
+		public int Size { get; set; } = 10;
+
+		[JsonPropertyName("offset")]
+		public int Offset { get; set; } = 0;
+	}
+
+	public class SortBy {
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("isAscending")]
+		public bool IsAscending { get; set; } = false;
+	}
+
+	public class Variables {
+		[JsonPropertyName("id")]
+		public string ID { get; set; }
+
+		[JsonPropertyName("pageArgs")]
+		public PageArgs PageArgs { get; set; } = new();
+
+		[JsonPropertyName("sortBy")]
+		public SortBy SortBy { get; set; } = new();
+	}
+
+	public class GraphQLQuery {
+		[JsonPropertyName("operationName")]
+		public string OperationName { get; set; }
+
+		[JsonPropertyName("extensions")]
+		public Extensions Extensions { get; set; } = new();
+
+		[JsonPropertyName("variables")]
+		public Variables Variables { get; set; } = new();
+
+		public GraphQLQuery() { }
+
+		public GraphQLQuery(string operationName, string hash, string variableID, string sorting) {
+			OperationName = operationName;
+			Extensions.PersistedQuery.Sha256Hash = hash;
+			Variables.ID = variableID;
+			Variables.SortBy.Name = sorting;
+		}
+	}
+}
---
+++ PSPlusMonthlyGames-Notifier/Models/GraphQL/GraphQLResponse.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PSPlusMonthlyGames_Notifier.Models.GraphQL {
+	public class Price {
+		[JsonPropertyName("basePrice")]
+		public string BasePrice { get; set; }
+
+		[JsonPropertyName("includesBundleOffer")]
+		public bool IncludesBundleOffer { get; set; }
+
+		[JsonPropertyName("isExclusive")]
+		public bool IsExclusive { get; set; }
+
+		[JsonPropertyName("isFree")]
+		public bool IsFree { get; set; }
+
+		[JsonPropertyName("isTiedToSubscription")]
+		public bool IsTiedToSubscription { get; set; }
+
+		[JsonPropertyName("upsellServiceBranding")]
+		public List<string> UpsellServiceBranding { get; set; }
+
+		[JsonPropertyName("upsellText")]
+		public string UpsellText { get; set; }
+	}
+
+	public class Concept {
+		[JsonPropertyName("id")]
+		public string ID { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("price")]
+		public Price Price { get; set; }
+	}
+
+	public class PageInfo {
+		[JsonPropertyName("offset")]
+		public int Offset { get; set; }
+
+		[JsonPropertyName("size")]
+		public int Size { get; set; }
+
+		[JsonPropertyName("totalCount")]
+		public int TotalCount { get; set; }
+	}
+
+	public class CategoryGridRetrieve {
+		[JsonPropertyName("concepts")]
+		public List<Concept> Concepts { get; set; }
+
+		[JsonPropertyName("pageInfo")]
+		public PageInfo PageInfo { get; set; }
+	}
+
+	public class Data {
+		[JsonPropertyName("categoryGridRetrieve")]
+		public CategoryGridRetrieve CategoryGridRetrieve { get; set; }
+	}
+
+	public class GraphQLResponse {
+		[JsonPropertyName("data")]
+		public Data Data { get; set; }
+	}
+}
