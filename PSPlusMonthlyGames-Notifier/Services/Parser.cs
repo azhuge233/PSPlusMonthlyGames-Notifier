ef87c94db7566c14a4753b8a1ddefd3f02cb8646
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2026-01-08 13:14:47 +0800

    Update parser for new info sourse StoreAPI.

--- PSPlusMonthlyGames-Notifier/Services/Parser.cs
+++ PSPlusMonthlyGames-Notifier/Services/Parser.cs
@@ -5,22 +5,26 @@ using PSPlusMonthlyGames_Notifier.Models.Record;
 using PSPlusMonthlyGames_Notifier.Models.Config;
 using System.Text;
 using Microsoft.Extensions.Options;
+using System.Text.Json;
+using PSPlusMonthlyGames_Notifier.Models.GraphQL;
 
 namespace PSPlusMonthlyGames_Notifier.Services {
 	internal class Parser(ILogger<Parser> logger, IOptions<Config> config, Scraper scraper) : IDisposable {
 		private readonly ILogger<Parser> _logger = logger;
 		private readonly Config config = config.Value;
 
-		public Tuple<List<FreeGameRecord>, List<FreeGameRecord>> Parse(HtmlDocument htmlDoc, List<FreeGameRecord> oldRecords) {
+		public async Task<Tuple<List<FreeGameRecord>, List<FreeGameRecord>>> Parse(string source, List<FreeGameRecord> oldRecords) {
 			try {
 				_logger.LogDebug(ParseString.debugHtmlParser);
 
 				Tuple<List<FreeGameRecord>, List<FreeGameRecord>> result = null;
 
 				if (config.InfoSource.ToLower() == "psblog")
-					result = ParsePSBlog(htmlDoc, oldRecords);
+					result = await ParsePSBlog(source, oldRecords);
 				else if (config.InfoSource.ToLower() == "psnine")
-					result = ParsePSNine(htmlDoc, oldRecords);
+					result = await ParsePSNine(source, oldRecords);
+				else if (config.InfoSource.ToLower() == "storeapi")
+					result = ParseAPI(source, oldRecords);
 
 				_logger.LogDebug($"Done: {ParseString.debugHtmlParser}");
 				return result;
@@ -30,9 +34,12 @@ namespace PSPlusMonthlyGames_Notifier.Services {
 			}
 		}
 
-		private Tuple<List<FreeGameRecord>, List<FreeGameRecord>> ParsePSBlog(HtmlDocument htmlDoc, List<FreeGameRecord> oldRecords) {
+		private async Task<Tuple<List<FreeGameRecord>, List<FreeGameRecord>>> ParsePSBlog(string source, List<FreeGameRecord> oldRecords) {
 			try {
 				_logger.LogDebug(ParseString.debugParsePSBlog);
+
+				var htmlDoc = new HtmlDocument();
+				htmlDoc.LoadHtml(source);
 
 				var resultList = new List<FreeGameRecord>();
 				var pushList = new List<FreeGameRecord>();
@@ -52,7 +59,7 @@ namespace PSPlusMonthlyGames_Notifier.Services {
 						resultList.Add(oldRecords.First(record => record.Title == title));
 						_logger.LogDebug(ParseString.debugFoundInPreviousRecords, title);
 					} else {
-						var postContentSource = GetPostContent(link);
+						var postContentSource = await GetPostContent(link);
 						var subTitle = postContentSource.DocumentNode.SelectSingleNode(ParseString.PSBlogSubHeaderXPath).InnerText.Trim();
 
 						var freeGame = new FreeGameRecord() {
@@ -79,9 +86,12 @@ namespace PSPlusMonthlyGames_Notifier.Services {
 			}
 		}
 
-		private Tuple<List<FreeGameRecord>, List<FreeGameRecord>> ParsePSNine(HtmlDocument htmlDoc, List<FreeGameRecord> oldRecords) {
+		private async Task<Tuple<List<FreeGameRecord>, List<FreeGameRecord>>> ParsePSNine(string source, List<FreeGameRecord> oldRecords) {
 			try {
 				_logger.LogDebug(ParseString.debugParsePSNine);
+
+				var htmlDoc = new HtmlDocument();
+				htmlDoc.Load(source);
 
 				var resultList = new List<FreeGameRecord>();
 				var pushList = new List<FreeGameRecord>();
@@ -101,7 +111,8 @@ namespace PSPlusMonthlyGames_Notifier.Services {
 						resultList.Add(oldRecords.First(record => record.Title == title));
 						_logger.LogDebug(ParseString.debugFoundInPreviousRecords, title);
 					} else {
-						var contentDiv = GetPostContent(link).DocumentNode.SelectSingleNode(ParseString.PSNineContentXPath);
+						var postContentSource = await GetPostContent(link);
+						var contentDiv = postContentSource.DocumentNode.SelectSingleNode(ParseString.PSNineContentXPath);
 						var contents = contentDiv.SelectSingleNode(ParseString.PSNineContentBoldXPath);
 						var sb = new StringBuilder();
 
@@ -145,14 +156,59 @@ namespace PSPlusMonthlyGames_Notifier.Services {
 			}
 		}
 
-		private HtmlDocument GetPostContent(string url) {
+		private Tuple<List<FreeGameRecord>, List<FreeGameRecord>> ParseAPI(string data, List<FreeGameRecord> oldRecords) {
+			try {
+				_logger.LogDebug(ParseString.debugParseAPI);
+
+				var queryResp = JsonSerializer.Deserialize<GraphQLResponse>(data);
+				var catagoryRetrieve = queryResp.Data.CategoryGridRetrieve;
+				var monthlyFreeGames = catagoryRetrieve.Concepts.Where(c => c.Price.UpsellText.ToLower() == ParseString.PSPlusUpSellText).ToList();
+
+				_logger.LogInformation(ParseString.infoFoundFreeGames, catagoryRetrieve.PageInfo.TotalCount, catagoryRetrieve.Concepts.Count, monthlyFreeGames.Count);
+
+				var resultList = new List<FreeGameRecord>();
+				var pushList = new List<FreeGameRecord>();
+
+				if (monthlyFreeGames.Count > 0) {
+					foreach (var game in monthlyFreeGames) {
+						var gameName = game.Name;
+						var gamePrice = $"{ParseString.GamePricePrefix}{game.Price.BasePrice}";
+						var gameUrl = $"{ParseString.PSStoreGameUrlPrefix}{game.ID}";
+
+						var newRecord = new FreeGameRecord() {
+							Title = gameName,
+							SubTitle = gamePrice,
+							Url = gameUrl
+						};
+
+						resultList.Add(newRecord);
+
+						if (!oldRecords.Any(r => r == newRecord)) {
+							_logger.LogInformation(ParseString.infoAddArticleToList, newRecord.Title);
+							pushList.Add(newRecord);
+						} else _logger.LogDebug(ParseString.debugFoundInPreviousRecords, newRecord.Title);
+					}
+				} else _logger.LogDebug(ParseString.debugNoFreeGamesDetected);
+
+				_logger.LogDebug($"Done: {ParseString.debugParseAPI}");
+				return new Tuple<List<FreeGameRecord>, List<FreeGameRecord>>(resultList, pushList);
+			} catch (Exception) {
+				_logger.LogError($"Error: {ParseString.debugParseAPI}");
+				throw;
+			}
+		}
+
+		private async Task<HtmlDocument> GetPostContent(string url) {
 			try {
 				_logger.LogDebug(ParseString.debugGetPostContent, url);
 
-				var source = scraper.GetPostContent(url);
+				var source = await scraper.GetHtmlSource(url);
+
+				var htmlDoc = new HtmlDocument();
+				htmlDoc.LoadHtml(source);
 
 				_logger.LogDebug($"Done: {ParseString.debugGetPostContent}", url);
-				return source;
+				return htmlDoc;
 			} catch (Exception) {
 				_logger.LogError($"Error: {ParseString.debugGetPostContent}", url);
 				throw;
--- PSPlusMonthlyGames-Notifier/Strings/ParseString.cs
+++ PSPlusMonthlyGames-Notifier/Strings/ParseString.cs
@@ -14,9 +14,15 @@
 		#endregion
 
 		#region Post Page Related
-		internal static readonly List<string> PSBlogTitleKeywords = new() { "PlayStation Plus", "月", "遊戲" };
-		internal static readonly List<string> PSNineTitleKeyWords = new() { "港服", "PLUS", "限免" };
-		internal static readonly List<string> PSNineContentBreakKeywords = new() { "二档" };
+		internal static readonly List<string> PSBlogTitleKeywords = ["PlayStation Plus", "月", "遊戲"];
+		internal static readonly List<string> PSNineTitleKeyWords = ["港服", "PLUS", "限免"];
+		internal static readonly List<string> PSNineContentBreakKeywords = ["二档"];
+		#endregion
+
+		#region ps store api related
+		internal const string PSStoreGameUrlPrefix = "https://store.playstation.com/zh-hans-hk/concept/";
+		internal const string GamePricePrefix = "Original Price: ";
+		internal const string PSPlusUpSellText = "ps plus";
 		#endregion
 
 		internal const string removeSpecialCharsRegex = @"[^0-9a-zA-Z]+";
@@ -25,15 +31,18 @@
 		internal const string debugHtmlParser = "Parse";
 		internal const string debugParsePSBlog = "Parsing ps blog";
 		internal const string debugParsePSNine = "Parsing ps nine";
+		internal const string debugParseAPI = "Parsing ps store api";
 
 		internal const string debugGetPostContent = "Get Post Content - URL: {url}";
 
 		internal const string debugNoRecordDetected = "No record detected!";
+		internal const string debugNoFreeGamesDetected = "No free games detected!";
 		internal const string debugSkipBlog = "Non Monthly-Games-related article, skip: {title}";
 		internal const string debugArticleFound = "Found new article: {title}";
-		internal const string debugFoundInPreviousRecords = "{title} is found in previous records, stop adding in list";
+		internal const string debugFoundInPreviousRecords = "[{title}] is found in previous records, stop adding in list";
 
-		internal const string infoAddArticleToList = "Added article {title} to push list";
+		internal const string infoAddArticleToList = "Added article [{title}] to push list";
+		internal const string infoFoundFreeGames = "Found free games, total: {0}, fetched: {1}, monthly free games: {2}";
 		#endregion
 	}
 }
