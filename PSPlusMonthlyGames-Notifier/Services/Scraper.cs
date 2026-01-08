360b42d9d6ab6481b018c961f66c92db479415cb
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2026-01-08 13:14:36 +0800

    Update scraper for new info sourse StoreAPI.

--- PSPlusMonthlyGames-Notifier/Services/Scraper.cs
+++ PSPlusMonthlyGames-Notifier/Services/Scraper.cs
@@ -1,41 +1,184 @@
-﻿using HtmlAgilityPack;
+﻿// using HtmlAgilityPack;
+using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Playwright;
 using PSPlusMonthlyGames_Notifier.Models.Config;
+using PSPlusMonthlyGames_Notifier.Models.GraphQL;
 using PSPlusMonthlyGames_Notifier.Strings;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Web;
 
 namespace PSPlusMonthlyGames_Notifier.Services {
     internal class Scraper(ILogger<Scraper> logger, IOptions<Config> config) : IDisposable {
 		private readonly ILogger<Scraper> _logger = logger;
 		private readonly Config config = config.Value;
 
-		public HtmlDocument GetSource() {
+		public async Task<string> GetSource() {
+			if (config.InfoSource.ToLower() == "storeapi") return await GetAPISource();
+			else return await GetHtmlSource();
+		}
+
+		public async Task<string> GetHtmlSource() {
 			string url = ScrapeString.UrlMap[config.InfoSource.ToLower()];
-			_logger.LogDebug(ScrapeString.debugGetPageSource, url);
+			_logger.LogDebug(ScrapeString.debugGetHtmlSource, url);
 
 			try {
-				var webGet = new HtmlWeb();
-				var htmlDoc = webGet.Load(url);
-				_logger.LogDebug($"Done: {ScrapeString.debugGetPageSource}", url);
-				return htmlDoc;
+				var client = GetHttpClient();
+
+				var resp = await client.GetAsync(url);
+
+				var result = await resp.Content.ReadAsStringAsync();
+
+				_logger.LogDebug($"Done: {ScrapeString.debugGetHtmlSource}", url);
+				return result;
 			} catch (Exception) {
-				_logger.LogError($"Error: {ScrapeString.debugGetPageSource}", url);
+				_logger.LogError($"Error: {ScrapeString.debugGetHtmlSource}", url);
 				throw;
 			}
 		}
 
-		public HtmlDocument GetPostContent(string url) {
-			_logger.LogDebug(ScrapeString.debugGetPageSource, url);
+		public async Task<string> GetHtmlSource(string url) {
+			_logger.LogDebug(ScrapeString.debugGetHtmlSource, url);
 
 			try {
-				var webGet = new HtmlWeb();
-				var htmlDoc = webGet.Load(url);
-				_logger.LogDebug($"Done: {ScrapeString.debugGetPageSource}", url);
-				return htmlDoc;
+				var client = GetHttpClient();
+
+				var resp = await client.GetAsync(url);
+
+				var result = await resp.Content.ReadAsStringAsync();
+
+				_logger.LogDebug($"Done: {ScrapeString.debugGetHtmlSource}", url);
+				return result;
 			} catch (Exception) {
-				_logger.LogError($"Error: {ScrapeString.debugGetPageSource}", url);
+				_logger.LogError($"Error: {ScrapeString.debugGetHtmlSource}", url);
 				throw;
 			}
+		}
+
+		public async Task<string> GetAPISource() {
+			string url = ScrapeString.PSAPIGraphQLUrl;
+			_logger.LogDebug(ScrapeString.debugGetGraphQLSource);
+
+			try {
+				var client = GetHttpClient(true);
+
+				var query = new GraphQLQuery(
+					ScrapeString.GraphQLCategoryGridRetrieveOperationName,
+					ScrapeString.GraphQLCategoryGridRetrieveHashDefault,
+					ScrapeString.GraphQLCategoryGridRetrieveVariablesID,
+					ScrapeString.GraphQLCategoryGridRetrieveSortName
+				);
+
+				var stringContent = new StringContent(JsonSerializer.Serialize(query), System.Text.Encoding.UTF8, "application/json");
+				var resp = await client.PostAsync(url, stringContent);
+
+				if (!resp.IsSuccessStatusCode) {
+					_logger.LogDebug(ScrapeString.debugGetResponseWithDefaultHashFailed);
+
+					var newHash = await GetHash();
+					query.Extensions.PersistedQuery.Sha256Hash = newHash;
+
+					stringContent = new StringContent(JsonSerializer.Serialize(query), System.Text.Encoding.UTF8, "application/json");
+
+					resp = await client.PostAsync(url, stringContent);
+
+					if (!resp.IsSuccessStatusCode) throw new Exception(ScrapeString.debugGetResponseWithNewHashFailed);
+
+					_logger.LogDebug(ScrapeString.debugGetResponseWithNewHashSuccess);
+				}
+
+				var result = await resp.Content.ReadAsStringAsync();
+
+				_logger.LogDebug($"Done: {ScrapeString.debugGetGraphQLSource}");
+				return result;
+			} catch (Exception) {
+				_logger.LogError($"Error: {ScrapeString.debugGetGraphQLSource}");
+				throw;
+			}
+		}
+
+		private HttpClient GetHttpClient(bool isForAPI = false) { 
+			var client = new HttpClient();
+
+			client.DefaultRequestHeaders.Add(ScrapeString.UserAgentKey, ScrapeString.UserAgent);
+
+			if (isForAPI) {
+				client.DefaultRequestHeaders.Add(ScrapeString.RefererKey, ScrapeString.Referer);
+				client.DefaultRequestHeaders.Add(ScrapeString.XPSNLocaleKey, ScrapeString.XPSNLocale);
+				client.DefaultRequestHeaders.Add(ScrapeString.XPSNAppVersionKey, ScrapeString.XPSNAppVersion);
+			}
+
+			return client;
+		}
+
+		private async Task<string> GetHash() { 
+			var urlContainsHash = await GetHashUrlWithPlaywright();
+			return ExtractSha256HashFromUrl(urlContainsHash);
+		}
+
+		private async Task<string> GetHashUrlWithPlaywright() {
+			_logger.LogDebug(ScrapeString.debugGetHashUrlWithPlayright);
+			try {
+				// install firefox if not installed
+				Microsoft.Playwright.Program.Main(["install", "firefox"]);
+
+				// create playwright
+				using var playwright = await Playwright.CreateAsync();
+				await using var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions {
+					Headless = true
+				});
+
+				var context = await browser.NewContextAsync();
+				var page = await context.NewPageAsync();
+
+				await page.GotoAsync(ScrapeString.PSStoreLandingPageUrl);
+
+				// click accept cookie banner
+				var acceptButton = page.Locator(ScrapeString.BannerDeclineButtonSelector, new PageLocatorOptions { 
+					HasTextRegex = new Regex(ScrapeString.BannerDeclineButtonText) 
+				});
+				await acceptButton.WaitForAsync();
+				await acceptButton.ClickAsync();
+
+				// navigate to browse page, triggering graphql request
+				var button = page.Locator(ScrapeString.BrowseLinkSelector, new PageLocatorOptions { 
+					HasTextRegex = new Regex(ScrapeString.BrowseLinkText) 
+				});
+				await button.WaitForAsync();
+				await button.ClickAsync();
+
+				await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+				// detect the graphql request
+				var request = await page.WaitForRequestAsync(request => request.Url.Contains(ScrapeString.GraphQLCategoryGridRetrieveOperationName));
+
+				string matchedUrl = request.Url;
+
+				await browser.CloseAsync();
+
+				_logger.LogDebug($"Done: {ScrapeString.debugGetHashUrlWithPlayright}");
+				return matchedUrl;
+			} catch (Exception ex) {
+				_logger.LogError(ex, $"Error: {ScrapeString.debugGetHashUrlWithPlayright}");
+				throw;
+			}
+		}
+
+		private string ExtractSha256HashFromUrl(string url) {
+			url = HttpUtility.UrlDecode(url);
+
+			_logger.LogDebug(url);
+
+			var match = Regex.Match(url, ScrapeString.HashRegexPattern);
+
+			if (match.Success && match.Groups.Count > 1) {
+				var value = match.Groups[1].Value;
+				_logger.LogDebug(ScrapeString.debugGetHashFromUrlSuccess, value);
+				return value;
+			}
+
+			throw new Exception(ScrapeString.debugGetHashFromUrlFailed);
 		}
 
 		public void Dispose() {
--- PSPlusMonthlyGames-Notifier/Strings/ScrapeString.cs
+++ PSPlusMonthlyGames-Notifier/Strings/ScrapeString.cs
@@ -1,13 +1,54 @@
 ﻿namespace PSPlusMonthlyGames_Notifier.Strings {
     internal class ScrapeString {
         #region debug strings
-        internal const string debugGetPageSource = "Get source: {0} ";
-        #endregion
+        internal const string debugGetHtmlSource = "Get html source: {0} ";
+        internal const string debugGetGraphQLSource = "Get GraphQL source";
+		internal const string debugGetResponseWithDefaultHashFailed = "Get response with default hash failed, fetching new hash";
+		internal const string debugGetResponseWithNewHashFailed = "Get response with new hash failed, abort.";
+		internal const string debugGetResponseWithNewHashSuccess = "Get response with new hash success.";
+		internal const string debugGetHashUrlWithPlayright = "Get hash url with playwright";
+		internal const string debugGetHashFromUrlSuccess = "Get hash from url success, hash: {0}";
+		internal const string debugGetHashFromUrlFailed = "Get hash from url failed, abort.";
+		#endregion
 
-        internal const string PSBlogUrl = "https://blog.zh-hant.playstation.com/category/ps-plus/";
+		#region URL strings
+		internal const string PSBlogUrl = "https://blog.zh-hant.playstation.com/category/ps-plus/";
         internal const string PSNineUrl = "https://psnine.com/psnid/sakauenachi/topic";
+		internal const string PSStoreLandingPageUrl = "https://store.playstation.com/zh-hans-hk/pages/latest";
+		internal const string PSAPIGraphQLUrl = "https://web.np.playstation.com/api/graphql/v1/op";
+		#endregion
 
-        internal static readonly Dictionary<string, string> UrlMap = new() {
+		#region graphql query strings
+		internal const string GraphQLCategoryGridRetrieveOperationName = "categoryGridRetrieve";
+		internal const string GraphQLCategoryGridRetrieveHashDefault = "257713466fc3264850aa473409a29088e3a4115e6e69e9fb3e061c8dd5b9f5c6";
+        internal const string GraphQLCategoryGridRetrieveVariablesID = "28c9c2b2-cecc-415c-9a08-482a605cb104";
+        internal const string GraphQLCategoryGridRetrieveSortName = "contentCollections.contentCollectionStartDate";
+		#endregion
+
+		#region http client header strings
+		internal const string UserAgentKey = "User-Agent";
+		internal const string RefererKey = "Referer";
+        internal const string XPSNLocaleKey = "x-psn-store-locale-override";
+        internal const string XPSNAppVersionKey = "x-psn-app-ver";
+
+		internal const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0";
+		internal const string Referer = "https://store.playstation.com/";
+        internal const string XPSNLocale = "zh-Hans-HK";
+        internal const string XPSNAppVersion = "/0.0.0-";
+		#endregion
+
+		#region selector strings
+		internal const string BannerDeclineButtonSelector = "button";
+		internal const string BannerDeclineButtonText = "Decline";
+		internal const string BrowseLinkSelector = "ul#tertiary-menu-toggle > li > a";
+		internal const string BrowseLinkText = "浏览";
+		#endregion
+
+		#region regex patterns
+		internal const string HashRegexPattern = @"""sha256Hash""\s*:\s*""([a-fA-F0-9]{64})""";
+		#endregion
+
+		internal static readonly Dictionary<string, string> UrlMap = new() {
             { "psblog", PSBlogUrl }, { "psnine", PSNineUrl }
         };
 	}
